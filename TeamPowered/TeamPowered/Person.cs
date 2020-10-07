27eac92e90f8b2aca9b0ce423519b3d968bf5dc8
GCarrell <GCarrell@users.noreply.github.com>
2020-10-07 15:59:28 +0100

    Add new properties for person class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace TeamPowered
 {
     class Person
     {
+        public string name(Get; set; )
+        public string Address ( get; set; )  
     }
 }
